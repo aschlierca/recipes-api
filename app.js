eba469520866bccf2bedbca64f9ea2634619651f
aschlierca <chosseo.yeon@gmail.com>
2026-06-24 11:40:06 -0400

    Part 8: CORS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const apiRouter = require("./api");
 
 const app = express();
 app.use(morgan("dev"));
 
+app.use(cors());
 
 app.use(express.json());
 
